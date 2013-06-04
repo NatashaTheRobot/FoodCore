071a1d69307b2b5b6e0e4cefc1c4319fc6138937
Natasha Murashev <nmurashev@gmail.com>
2013-06-04 18:01:43 -0500

    first commit

---
+++ FoodCore/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  FoodCore
+//
+//  Created by Natasha Murashev on 6/4/13.
+//  Copyright (c) 2013 Natasha Murashev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
